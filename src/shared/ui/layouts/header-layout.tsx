6262ec61362361e1241137b5689fa045abf85657
Moshniaga Artem <artem.m89@mail.ru>
2024-11-27 21:26:25 +0300

    fix: add container in header

--- src/shared/ui/layouts/header-layout.tsx
+++ src/shared/ui/layouts/header-layout.tsx
@@ -1,9 +1,9 @@
 'use client';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { Container } from './container';
 
-const HeaderStyled = styled.header`
-  position: relative;
+const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -13,29 +13,47 @@ const HeaderStyled = styled.header`
   background-color: hsl(19 31% 16%);
 `;
 
+const HeaderWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const HomeLink = styled(Link)`
-  position: absolute;
-  top: 17px;
-  left: 20px;
+  min-width: 50px;
+  display: flex;
+  align-items: center;
   color: inherit;
+`;
+
+const Logo = styled.div`
+  flex-basis: '100%';
+  display: 'flex';
+  justify-content: 'center';
 `;
 
 export const HeaderLayout = ({ logo }: { logo: string }) => {
   return (
-    <HeaderStyled>
-      <HomeLink href={'/users'}>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'>
-          <path
-            d='M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z'
-            fill='currentColor'
-          />
-        </svg>
-      </HomeLink>
-      <div>{logo}</div>
-    </HeaderStyled>
+    <Header>
+      <Container style={{ width: '100%' }}>
+        <HeaderWrapper>
+          <HomeLink href={'/users'}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'>
+              <path
+                d='M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z'
+                fill='currentColor'
+              />
+            </svg>
+          </HomeLink>
+          <Logo>{logo}</Logo>
+          <div style={{ minWidth: '50px' }}></div>
+        </HeaderWrapper>
+      </Container>
+    </Header>
   );
 };
